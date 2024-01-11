00f84180c44d13e9a0a3d8eeb085c68f2a87bac2
Jasmine <jasminetumulak@gmail.com>
2024-01-11 23:43:18 +0400

    deploy

--- client/src/Config/config.js
+++ client/src/Config/config.js
@@ -1,4 +1,4 @@
 //local
-export const URL = "http://localhost:3002";
+//export const URL = "http://localhost:3002";
 //production
-//export const URL = "https://mern-studapp.onrender.com";
+export const URL = "https://postit-jxdk.onrender.com/";
